d35fc1b75e2a308dcf0354f5d6a1c6045dcd6847
Vincent Jousse <vincent@jousse.org>
2018-03-10 08:48:00 +0100

    Functional programming style for search function

--- src/lib.rs
+++ src/lib.rs
@@ -38,16 +38,13 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
+    let query: String = query.to_lowercase();
 
-    results
+    contents
+        .lines()
+        .filter(|c| c.to_lowercase().contains(&query))
+        .collect()
 }
 
 pub fn run(config: Config) -> Result<(), Box<Error>> {
